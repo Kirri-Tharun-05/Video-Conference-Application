4285dc3dd82fee0c40d9762d7f5d32cf590f7fbd
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-03-11 13:36:52 +0530

    Updated

--- Frontend/src/components/Room.jsx
+++ Frontend/src/components/Room.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import isAuth from '../utils/isAuth'
-function Room(){
-    const navigate=useNavigate();
-    const [roomID,setRoomId]=useState();
-    const handleClick=()=>{
-        navigate(`/Room/${roomID}`);
-    }
+import Lottie from 'lottie-react'
+import animation1 from '../assets/animation1.json'
+function Room() {
+  const navigate = useNavigate();
+  const [roomID, setRoomId] = useState();
+  const handleClick = () => {
+    navigate(`/Room/${roomID}`);
+  }
   return (
     <div>
-        <h1>Hello User!</h1>
-        <h1>Type Your Room Id</h1>
-        <input type="text" name="" id="" placeholder='Enter Room Id' onChange={e=>setRoomId(e.target.value)}/>
-        <button type='button' onClick={handleClick}>Join</button>
+      <div className="roomContent">
+        <div>
+          <h1 className='text-3xl'>Hello User!</h1>
+          <h1 className='text-2xl'>Type Your Room Id</h1>
+          <input type="text" name="" id="" placeholder='Enter Room Id' onChange={e => setRoomId(e.target.value)} className='roomId'/>
+          <button type='button' onClick={handleClick}>Join</button>
+        </div>
+        <div className="">
+          <Lottie animationData={animation1}/>
+        </div>
+      </div>
     </div>
   )
 }
